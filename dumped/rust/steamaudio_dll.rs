af0bda5eee5d38844af38f60332bf05947ddb79a
ellii <patrick@savells.net>
2025-11-15 11:40:42 -0600

    Updating steamaudio_dll.rs

--- dumped/rust/steamaudio_dll.rs
+++ dumped/rust/steamaudio_dll.rs
@@ -1,5 +1,5 @@
 // dumped with csdumper (created by shxdow)
-// 2025-10-18 16:17:55 UTC
+// 2025-11-15 17:40:41 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 

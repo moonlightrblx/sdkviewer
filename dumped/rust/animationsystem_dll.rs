80d85b52cc689ed98069ef4bcaa716922235b9ef
ellii <patrick@savells.net>
2025-11-22 14:47:27 -0600

    Updating animationsystem_dll.rs

--- dumped/rust/animationsystem_dll.rs
+++ dumped/rust/animationsystem_dll.rs
@@ -1,5 +1,5 @@
 // dumped with csdumper (created by shxdow)
-// 2025-11-21 02:58:05 UTC
+// 2025-11-22 20:47:26 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
